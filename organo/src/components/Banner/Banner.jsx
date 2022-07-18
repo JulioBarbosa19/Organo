67c655d64c7ccab46e1150bf88b057bddfc4aec9
JulioBarbosa19 <juliocesarb.silva11@gmail.com>
2022-07-18 18:35:16 -0300

    initial commit + feat Banner

---
+++ organo/src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+const Banner = () =>{
+    //jsx
+    return (
+        <header className="banner">
+        <img src="/imagens/banner.png" alt="O banner principal da página Organo" />
+        </header>
+    )
+}
+
+export default Banner
