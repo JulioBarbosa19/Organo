14690944f21d02598617393def756e24238cb195
JulioBarbosa19 <juliocesarb.silva11@gmail.com>
2022-07-20 21:44:41 -0300

    delete some archives and create component Input

---
+++ organo/src/components/Input/index.jsx
@@ -0,0 +1,12 @@
+import './Input.css';
+
+const Input = () => {
+     return (
+        <div className="input-text">
+            <label>Nome</label>
+            <input placeholder='Digite o seu nome' />
+        </div>
+     )
+}
+
+export default Input;
